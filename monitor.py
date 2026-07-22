6a7da27630c19cfa349ea66c527184e7e80d2569
youdlkkkku0401-ship-it <youdlkkkku0401@gmail.com>
2026-07-22 22:11:21 +0900

    Update monitor.py

--- monitor.py
+++ monitor.py
@@ -174,6 +174,7 @@ def check_subscriber_increases():
         #データ取得
         channel_data = subscriber_data["channels"].get(channel_name, {})
         if increase > 0:
+            last_posted_day = subscriber_data["channels"].get(channel_name, {}).get("last_posted_day")
             #10万人突破
             if previous_subscribers // 100000  < current_subscribers // 100000:
                 current_million = current_subscribers // 100000 * 10
@@ -183,9 +184,9 @@ def check_subscriber_increases():
             elif previous_subscribers // 10000  < current_subscribers // 10000:
                 current_man = current_subscribers // 10000
                 post_content = f"#{channel_name} さんが登録者 {current_man}万人 に到達しました"
-    
+
             #2K以上または奇数の時
-            elif (over2K or current_subscribers%2000 == 1000):
+            elif (over2K or current_subscribers%2000 == 1000) and (datetime.now() - last_post_day >= timedelta(hours=1)):
             #elif (over2K or current_subscribers % 2000 == 0):
                 #投稿差分計算(+***人
                 last_post_fan = subscriber_data["channels"].get(channel_name, {}).get("last_post_fan")
@@ -195,7 +196,6 @@ def check_subscriber_increases():
                     post_increase = increase
     
                 #日付計算/*日)
-                last_posted_day = subscriber_data["channels"].get(channel_name, {}).get("last_posted_day")
                 if last_posted_day:
                     last_posted_dt = datetime.fromisoformat(last_posted_day)
                     delta = datetime.now() - last_posted_dt
