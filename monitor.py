3f692bbed3f77061ad6d9e0add563d617fefc26b
youdlkkkku0401-ship-it <youdlkkkku0401@gmail.com>
2026-09-02 09:32:59 +0900

    Update monitor.py

--- monitor.py
+++ monitor.py
@@ -236,7 +236,7 @@ def check_subscriber_increases():
             if current_subscribers%1000==0:
                 if not post_content:
                     send_discord_notification(f"更新：{channel_name}：{current_subscribers}人(＋{increase}人）")
-                    if can_post and not pending_post:
+                    if not pending_post:
                         channel_data["last_posted_day"] = datetime.now().isoformat()
                         channel_data["last_post_fan"] = current_subscribers
     
